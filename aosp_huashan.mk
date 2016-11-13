deepongi/android_device_sony_huashan

huashan: Add AOSP build support
Change-Id: I49ec15dddc775ce5d294b33e4846b7382176e402
Signed-off-by: AdrianDC <radian.dc@gmail.com>

aosp_huashan.mk
@@ -1,0 +1,33 @@
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit AOSP common.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/sony/huashan/huashan.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifications
+PRODUCT_DEVICE := huashan
+PRODUCT_NAME := aosp_huashan
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia SP
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"

AOSP-RRO/device_sony_huashan

aosp_huashan.mk
@@ -1,0 +1,33 @@
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit AOSP common.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/sony/huashan/device.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifications
+PRODUCT_DEVICE := huashan
+PRODUCT_NAME := aosp_huashan
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia SP
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"